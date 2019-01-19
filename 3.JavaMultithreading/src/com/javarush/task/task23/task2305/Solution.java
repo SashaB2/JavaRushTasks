6c4d94c1665ca3239882a18fc3aa0f8eb2ea0e73
SashaB2 <sviridenko1@gmail.com>
2019-01-19 21:01:44 +0200

    23**

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Inner
+*/
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution [] solutions = {new Solution(), new Solution()};
+        solutions[0].innerClasses = new InnerClass[]{new Solution().new InnerClass(), new Solution().new InnerClass()};
+        solutions[1].innerClasses = new InnerClass[]{new Solution().new InnerClass(), new Solution().new InnerClass()};
+
+        return solutions;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
