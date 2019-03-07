e5326715acc05a2c34128b0d8441fa727f595612
SashaB2 <sviridenko1@gmail.com>
2019-03-07 12:06:57 +0200

    Chat_13

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,7 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
@@ -10,5 +14,34 @@ public class Client {
 
     public class SocketThread extends Thread{
 
+    }
+
+    protected String getServerAddress(){
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Error on client side to send message");
+            clientConnected = false;
+        }
     }
 }
